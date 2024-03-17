4a8cca0a2bc3afdaa2f68e9df5a166a74f7ceedd
ChristianGalindo10 <cygc1@hotmail.com>
2024-03-17 16:38:12 -0500

    FEAT: winners field added to form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -13,7 +13,8 @@ const CustomForm = (props) => {
         title: '',
         description: '',
         image: '',
-        state: ''
+        state: false,
+        winners: 1
     }
     if (readOnlyProp && props.data) {
         initial_data = props.data;
@@ -59,107 +60,6 @@ const CustomForm = (props) => {
         }
     }
 
-    // useEffect(() => {
-    //     async function fechtData() {
-    //         console.log("haciendo peticion")
-    //         const code = new URLSearchParams(window.location.search).get('code');
-    //         const CLIENT_ID = '1218718388809891841';
-    //         const CLIENT_SECRET = 'NmMU9hIzlnC18bs3qToZDFAlxg6H1BF8';
-    //         let options = {
-    //             url: 'https://discord.com/api/oauth2/token',
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/x-www-form-urlencoded'
-    //             },
-    //             body: new URLSearchParams({
-    //                 'client_id': CLIENT_ID,
-    //                 'client_secret': CLIENT_SECRET,
-    //                 'grant_type': 'authorization_code',
-    //                 'code': code,
-    //                 'redirect_uri': 'http://localhost:5173/principal'
-    //             })
-    //         }
-    //         const data = await fetch('https://discord.com/api/v10/oauth2/token', options);
-    //         const response = await data.json();
-    //         console.log(response)
-    //         const accessToken = response.access_token;
-    //         const tokenType = response.token_type;
-    //         sessionStorage.setItem("tokenType", tokenType);
-    //         sessionStorage.setItem("accessToken", accessToken);
-    //         if (!accessToken) {
-    //             // window.location.href = '/';
-    //         }
-
-    //         fetch('https://discord.com/api/users/@me/guilds', {
-    //             headers: {
-    //                 authorization: `${tokenType} ${accessToken}`,
-    //             },
-    //         })
-    //             .then(result => result.json())
-    //             .then(response => {
-    //                 console.log(response);
-    //             })
-    //             .catch(console.error);
-
-    //         fetch('https://discord.com/api/users/@me', {
-    //             headers: {
-    //                 authorization: `${tokenType} ${accessToken}`,
-    //             },
-    //         })
-    //             .then(result => result.json())
-    //             .then(response => {
-    //                 console.log(response);
-    //                 const { username, discriminator, avatar, id } = response;
-    //                 //set the welcome username string
-    //                 setData(response);
-    //                 sessionStorage.setItem('userData', JSON.stringify(response));
-    //             })
-    //             .catch(console.error);
-    //     }
-    //     if (window.location.pathname === "/principal") {
-    //         if (!sessionStorage.getItem("accessToken")) {
-    //             fechtData();
-    //         }
-    //     }
-
-    // }, []);
-
-    // const [isLoggedIn, setIsLoggedIn] = useState(false);
-    // const [userName, setUserName] = useState('');
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     const loggedInUserString = sessionStorage.getItem('userData');
-    //     if (!loggedInUserString) {
-    //         setIsLoggedIn(false);
-    //         setUserName('');
-    //     } else {
-    //         const loggedInUser = JSON.parse(loggedInUserString);
-    //         setIsLoggedIn(true);
-    //         setUserName(loggedInUser.name);
-    //     }
-
-    // }, [sessionStorage.getItem('userData')]);
-
-    // const handleLogin = () => {
-    //     const clientId = '1218718388809891841';
-    //     const redirectUri = encodeURIComponent('http://localhost:5173/principal');
-    //     const scope = encodeURIComponent('identify guilds');
-    //     const authorizationUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fprincipal&scope=guilds+identify`;
-    //     window.location.href = authorizationUrl;
-    // };
-
-    // const handleLogout = () => {
-    //     sessionStorage.clear();
-    //     setIsLoggedIn(false);
-    //     navigate('/', { replace: true });
-    //     Swal.fire({
-    //         icon: 'success',
-    //         title: '¡Logout!',
-    //         text: 'Cierre de sesión existoso.',
-    //     });
-    // };
-
     return (
         <>
             <div className="col-12 col-sm-9 col-md-5 mt-3 mb-5 mx-auto">
@@ -201,7 +101,13 @@ const CustomForm = (props) => {
                                     Fill out this field
                                 </div>
                             </div>
-
+                            <div className="mb-3">
+                                <label htmlFor="description" className="form-label">Number of winners</label>
+                                <input className="form-control" type="number" min={1} id='winners' onChange={handleInput} readOnly={readOnlyProp} value={data.winners} required />
+                                <div class="invalid-feedback">
+                                    Fill out this field
+                                </div>
+                            </div>
                             <div className="mb-3">
                                 <label htmlFor="start_date" className="form-label">Giveaway state</label>
                                 <div className="form-check form-switch">
